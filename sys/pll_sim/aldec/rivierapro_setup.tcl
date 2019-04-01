b2d116f8256f1549883c4fe6d208310731c2be25
davewoo999 <dave.a.wood@gmail.com>
2019-04-01 16:59:07 +0100

    newer version

--- sys/pll_sim/aldec/rivierapro_setup.tcl
+++ sys/pll_sim/aldec/rivierapro_setup.tcl
@@ -1,5 +1,5 @@
 
-# (C) 2001-2018 Altera Corporation. All rights reserved.
+# (C) 2001-2019 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 17.1 593 win32 2018.09.21.09:07:11
+# ACDS 17.1 593 win32 2019.02.20.15:40:15
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
